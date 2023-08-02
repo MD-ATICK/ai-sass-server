c0d1d45c36ee6aa0d3ec26cf402ae5769b4d2b60
Your Name <you@example.com>
2023-08-03 01:13:12 +0600

    how

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ const Stripe = require('stripe')
 const stripe = new Stripe(process.env.SRIPE_KEY)
 
 
-app.use(cors({
-    origin: 'https://lambent-kringle-2c65aa.netlify.app',
-    credentials: true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
